75a3024fa1f2adaae02dcdf246f0741d361399e6
Joy Nyayieka <67579434+JoyNyayieka@users.noreply.github.com>
2024-05-02 14:55:35 +0300

    First release of project

---
+++ I2CLCD.ino
@@ -0,0 +1,44 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+const int totalColumns = 16;
+const int totalRows = 2;
+
+LiquidCrystal_I2C lcd(0x27, totalColumns, totalRows);
+
+String staticMessage = "Group 6 members";
+String scrollingMessage[5] = {
+  "Carol Atieno - Member",
+  "Hope Prince - Member",
+  "Muthee Brain - Member",
+  "Joy Nyayieka - Member",
+  "Tole Rightson- Member"
+};
+
+void scrollMessage(int row, String message, int delayTime, int totalColumns) {
+  for (int i = 0; i < totalColumns; i++) {
+    message = " " + message;
+  }
+  message += " ";
+  for (int position = 0; position < message.length() - totalColumns; position++) {
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(position, position + totalColumns));
+    delay(delayTime);
+  }
+}
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  lcd.clear(); // Clear the display before printing
+  lcd.setCursor(0, 0);
+  lcd.print(staticMessage);
+  for (int i = 0; i < 5; i++) {
+    scrollMessage(1, scrollingMessage[i], 250, totalColumns);
+    delay(1000); // Delay between displaying each member's name
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,15 @@
+EMBEDDED SYSTEMS PROJECT ASSIGNMENT
+
+Tasks to undertake:
+Form algorithms/flowcharts for your project.
+Draw / Design a Circuit diagram/wiring Diagram of your project's circuit.
+Develop a suitable program/code capable of achieving/implementing your task. Comment on your code statements, if codes are copied, quote the source/libraries. Understand and be able to describe/explain the functionalities of the various components/devices you will use.
+
+PROJECT:
+I2C Based LCD Display. (Group Six) Task:
+Display the names of all members of your group sequentially, stating the roll of each member,
+Each member’s instance can be displayed for five seconds.
+Hardware provision:
+Arduino Boards / Raspberry PI, ESP8266, TWI converter, LCD Display, power supply, cable connectors.
+
+Course Instructor: Mr Mattew Thiong’o
